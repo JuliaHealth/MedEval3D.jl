31e3a135aef0a3546d7566f17feb7e93e0f114aa
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2021-09-03 14:58:04 +0200

    up

--- src/utils/structs/BasicStructs.jl
+++ src/utils/structs/BasicStructs.jl
@@ -3,9 +3,9 @@ using DrWatson
 
 module BasicStructs
 using Parameters
-```@doc
+"""
 constants associated with image over which we will evaluate segmentations
-```
+"""
 @with_kw struct ImageConstants
     mvspx ::double # Voxelspacing x 
     mvspy::double # Voxelspacing y
@@ -14,20 +14,18 @@ constants associated with image over which we will evaluate segmentations
     ZPositions::Vector{Double} # array of true physical positions (in mm) of slices relative to the begining - used in case we have variable thickness of slices
     numberOfVox::Int64 # number of voxels in image
 end #ImageConstants
-```@doc
+"""
 configuration struct that when passed will marks what kind of metrics we are intrested in 
     
-    ```
+"""
 @with_kw struct ConfigurtationStruct
     anyFuzzy::Bool = false# is any of the metric calculated fuzzy
-
-
 end #ConfigurtationStruct
 
 
-```@doc
+"""
 Struct holding all resulting metrics - if some metric was not calculated its value is just -1  
-```
+"""
 @with_kw struct ResultMetrics
     dice::Float64 = -1.0 #dice coefficient
     jaccard::Float64 =  -1.0 #jaccard coefficient
@@ -37,11 +35,16 @@ Struct holding all resulting metrics - if some metric was not calculated its val
     mi::Float64 = -1.0 # mutual information
     ic::Float64 = -1.0 # interclass correlation
     Kc::Float64 = -1.0 # Kohen Cappa
+end #ResultMetrics
 
-end #ConfigurtationStruct
+"""
+struct holding necess
+TN- true negative   TP - true positive  FN - false negative  FP - false positive
+"""
+@with_kw struct TnTpFpFn 
+look into https://github.com/JuliaArrays/StructArrays.jl
 
-
-
+end #TnTpFpFn
 
 
 
