87cb152b4a04741bad37d427837a007f2c519c4c
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2021-09-10 16:19:39 +0200

    working well after tidying up

--- src/playgrounds/mapReducePlayground.jl
+++ src/playgrounds/mapReducePlayground.jl
@@ -55,7 +55,74 @@ end#createAndInitializeShmem
 
 
 """
+reduction across the warp and adding to appropriate spots in the  shared memory
+"""
+function firstReduce(shmem,shmemSum,wid,threadIdx,lane   )
+    @inbounds sumFn = reduce_warp(shmem[threadIdx,1],32)
+    @inbounds sumFp = reduce_warp(shmem[threadIdx,2],32)
+    @inbounds sumTp = reduce_warp(shmem[threadIdx,3],32)
+ 
+    if(lane==1)
+   @inbounds shmemSum[wid,1]= sumFn
+     end  
+    if(lane==2) 
+       @inbounds shmemSum[wid,2]= sumFp
+    end     
+    if(lane==3)
+         @inbounds shmemSum[wid,3]= sumTp
+     end#if  
+end#firstReduce
+
+"""
+sets the final block amount of true positives, false positives and false negatives and saves it
+to the  array representing each slice, and to the shared memory so we will be able to also calculate some metrics in each block
+- those will be per slice metrics ...
+wid - the warp in a block we want to use
+numb - number associated with constant - used to access shared memory for example
+chosenWid - on which block we want to make a reduction to happen
+intermediateRes - array with intermediate -  slice wise results
+singleREs - the final  constant holding image witde values (usefull for example for debugging)
+shmemSum - shared memory where we get the  results to be reduced now and to which we will also save the output
+blockId - number related to block we are currently in 
+lane - the lane in the warp
+"""
+function getSecondBlockReduce(chosenWid,numb,wid, intermediateRes,singleREs,shmemSum,blockId,lane)
+    if(wid==chosenWid )
+        vall = reduce_warp(shmemSum[lane,numb],32 )
+        
+      #probably we do not need to sync warp as shfl dow do it for us         
+      if(lane==1)
+          @inbounds @atomic singleREs[]+=vall
+      end    
+      if(lane==2)
+        @inbounds intermediateRes[blockId]=vall
+      end    
+      if(lane==3)
+        #ovewriting the value 
+        @inbounds shmemSum[1,numb]=vall
+      end     
+
+  end  
+
+end#getSecondBlockReduce
+"""
+given appropriate constants related to slice like rate of true pasitives, fals positive, false negative
+and false negative
+it calculates wanted  segmentation metrics
+"""
+function calculateBasicMetricsPerSlice()
+
+
+end
+
+"""
 adapted from https://github.com/JuliaGPU/CUDA.jl/blob/afe81794038dddbda49639c8c26469496543d831/src/mapreduce.jl
+goldBoolGPU - array holding data of gold standard bollean array
+segmBoolGPU - boolean array with the data we want to compare with gold standard
+tp,tn,fp,fn - holding single values for true positive, true negative, false positive and false negative
+intermediateResTp, intermediateResFp, intermediateResFn - arrays holding slice wie results for true positive ...
+loopNumb - number of times the single lane needs to loop in order to get all needed data
+sliceEdgeLength - length of edge of the slice we need to square this number to get number of pixels in a slice
 """
 function getBlockTpFpFn(goldBoolGPU::CuDeviceArray{Bool,1, 1}
         , segmBoolGPU::CuDeviceArray{Bool, 1, 1}
@@ -64,7 +131,8 @@ function getBlockTpFpFn(goldBoolGPU::CuDeviceArray{Bool,1, 1}
         ,intermediateResFp::CuDeviceArray{Int32, 1, 1}
         ,intermediateResFn::CuDeviceArray{Int32, 1, 1}
         ,loopNumb::Int64
-        ,indexCorr::Int64)
+        ,indexCorr::Int64
+        ,sliceEdgeLength::Int64)
     # we multiply thread id as we are covering now 2 places using one lane - hence after all lanes gone through we will cover 2 blocks - hence second multiply    
     i = (threadIdx().x* indexCorr) + ((blockIdx().x - 1) *indexCorr) * (blockDim().x)# used as a basis to get data we want from global memory
    wid, lane = fldmod1(threadIdx().x,32)
@@ -78,52 +146,17 @@ function getBlockTpFpFn(goldBoolGPU::CuDeviceArray{Bool,1, 1}
     incr_shmem(threadIdx().x,goldBoolGPU[i+k],segmBoolGPU[i+k],shmem,blockIdx().x )
    end#for 
 
-   #reducing across the warp
-   @inbounds sumFn = reduce_warp(shmem[threadIdx().x,1],32)
-   @inbounds sumFp = reduce_warp(shmem[threadIdx().x,2],32)
-   @inbounds sumTp = reduce_warp(shmem[threadIdx().x,3],32)
+    #reducing across the warp
+    firstReduce(shmem,shmemSum,wid,threadIdx().x,lane)
+    sync_threads()
+    #now all data about of intrest should be in  shared memory so we will get all rsults from warp reduction in the shared memory 
+   
+    getSecondBlockReduce( 1,3,wid,intermediateResTp,tp,shmemSum,blockId,lane)
+    getSecondBlockReduce( 2,2,wid,intermediateResFp,fp,shmemSum,blockId,lane)
+    getSecondBlockReduce( 3,1,wid,intermediateResFn,fn,shmemSum,blockId,lane)
 
-   if(lane==1)
-  @inbounds shmemSum[wid,1]= sumFn
-    end  
-   if(lane==2) 
-      @inbounds shmemSum[wid,2]= sumFp
-   end     
-   if(lane==3)
-        @inbounds shmemSum[wid,3]= sumTp
-    end#if  
-    
-sync_threads()
-#now all data about of intrest should be in  shared memory so we will get all rsults from warp reduction in the shared memory
-    # in case we have only 32 warps as we set we will not go out of bounds
-    if(wid==2 )
-          vallTp = reduce_warp(shmemSum[lane,3],32 )
-        #probably we do not need to sync warp as shfl dow do it for us         
-        if(lane==1)
-            @inbounds @atomic tp[]+=vallTp
-            @inbounds intermediateResTp[blockId]=vallTp
-        end    
 
-    end  
-    
-    if(wid==3 )   
-        vallFp = reduce_warp(shmemSum[lane,2],32 )        
-
-        if(lane==1)
-            @inbounds @atomic fp[]+=vallFp
-            @inbounds intermediateResFp[blockId]=vallFp
-        end 
-    end 
-
-    if(wid==5)  
-            vallFn = reduce_warp(shmemSum[lane,1],32 )
-        if(lane==1)
-            @inbounds @atomic fn[]+=vallFn
-            @inbounds intermediateResFn[blockId]=vallFn
-        end  
-    end
-
-   return  
+    return  
    end
 
 """
