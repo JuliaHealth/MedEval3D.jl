20baac2ee2dbadf34e6fa85f3888090519f0ceb6
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2021-10-07 14:57:01 +0200

    up

--- src/distanceMetrics/MeansMahalinobis.jl
+++ src/distanceMetrics/MeansMahalinobis.jl
@@ -2,6 +2,7 @@
 
 module MeansMahalinobis
 using Main.BasicPreds, Main.CUDAGpuUtils, CUDA, Main.IterationUtils, Main.ReductionUtils, Main.MemoryUtils
+export meansMahalinobisKernel
 """
 IMPORTANT x dim of threadblock needs to be always 32 
 goldArr,segmArr  - arrays we analyze 
@@ -21,44 +22,43 @@ function meansMahalinobisKernel(goldArr,segmArr
     ,totalXGold,totalYGold,totalZGold,totalCountGold
     ,totalXSegm,totalYSegm,totalZSegm,totalCountSegm  )
 
-#summing coordinates of all voxels we are intrested in 
-sumX,sumY,sumZ = UInt64(0),UInt64(0),UInt64(0)
-#count how many voxels of intrest there are so we will get means
-count::UInt16 = UInt16(0)
-#for storing results from warp reductions
-shmemSum= @cuStaticSharedMem(UInt32, (32,4))   
-clearSharedMemWarpLong(shmemSum, UInt8(4))
-#just needed for reductions
-offsetIter = UInt8(1)
-#### first we analyze gold standard array
-@iter3d arrDims loopXdim loopYdim  loopZdim if(  @inbounds(goldArr[x,y,z])  ==numberToLooFor)
-       #updating variables needed to calculate means
-       sumX+=UInt64(x) ;  sumY+=UInt64(y)  ; sumZ+=UInt64(z)   ; count+=UInt16(1)   
- end#if bool in arr  
+    #summing coordinates of all voxels we are intrested in 
+    sumX,sumY,sumZ = UInt64(0),UInt64(0),UInt64(0)
+    #count how many voxels of intrest there are so we will get means
+    count::UInt16 = UInt16(0)
+    #for storing results from warp reductions
+    shmemSum= @cuStaticSharedMem(UInt32, (32,4))   
+    clearSharedMemWarpLong(shmemSum, UInt8(4))
+    #just needed for reductions
+    offsetIter = UInt8(1)
+    #### first we analyze gold standard array
+    @iter3d arrDims loopXdim loopYdim  loopZdim if(  @inbounds(goldArr[x,y,z])  ==numberToLooFor)
+        #updating variables needed to calculate means
+        sumX+=UInt64(x) ;  sumY+=UInt64(y)  ; sumZ+=UInt64(z)   ; count+=UInt16(1)   
+    end#if bool in arr  
 
-#tell what variables are to be reduced and by what operation
-@redWitAct(offsetIter,shmemSum,  sumX,+,     sumY,+    ,sumZ,+   ,count,+)
-#now we have needed values in  shmemSum[1,1] - sumX  shmemSum[1,2] - sumY shmemSum[1,3] - sumZ and in shmemSum[1,4] - offsetIter
-#important we need to send values to the atomics on the same warps as we did reduction 
-#so we can avoid thread synchronization in such situation
-@addAtomic(shmemSum,totalXGold, totalYGold ,totalZGold,totalCountGold)
+    #tell what variables are to be reduced and by what operation
+    @redWitAct(offsetIter,shmemSum,  sumX,+,     sumY,+    ,sumZ,+   ,count,+)
+    #now we have needed values in  shmemSum[1,1] - sumX  shmemSum[1,2] - sumY shmemSum[1,3] - sumZ and in shmemSum[1,4] - offsetIter
+    #important we need to send values to the atomics on the same warps as we did reduction 
+    #so we can avoid thread synchronization in such situation
+    @addAtomic(shmemSum,totalXGold, totalYGold ,totalZGold,totalCountGold)
 
-### now analyzing segmentation array
-#resetting variables
-sumX,sumY,sumZ = UInt64(0),UInt64(0),UInt64(0)
-count= UInt16(0)
-clearSharedMemWarpLong(shmemSum, UInt8(4))
-offsetIter = UInt8(1)
+    ### now analyzing segmentation array
+    #resetting variables
+    sumX,sumY,sumZ = UInt64(0),UInt64(0),UInt64(0)
+    count= UInt16(0)
+    clearSharedMemWarpLong(shmemSum, UInt8(4))
+    offsetIter = UInt8(1)
 
-@iter3d arrDims loopXdim loopYdim  loopZdim if(  @inbounds(segmArr[x,y,z])  ==numberToLooFor)
-    sumX+=UInt64(x) ;  sumY+=UInt64(y)  ; sumZ+=UInt64(z)   ; count+=UInt16(1)   
-end#if bool in arr  
-@redWitAct(offsetIter,shmemSum,  sumX,+,     sumY,+    ,sumZ,+   ,count,+)
-@addAtomic(shmemSum,totalXSegm, totalYSegm ,totalZSegm,totalCountSegm)
+    @iter3d arrDims loopXdim loopYdim  loopZdim if(  @inbounds(segmArr[x,y,z])  ==numberToLooFor)
+        sumX+=UInt64(x) ;  sumY+=UInt64(y)  ; sumZ+=UInt64(z)   ; count+=UInt16(1)   
+    end#if bool in arr  
+    @redWitAct(offsetIter,shmemSum,  sumX,+,     sumY,+    ,sumZ,+   ,count,+)
+    @addAtomic(shmemSum,totalXSegm, totalYSegm ,totalZSegm,totalCountSegm)
 
-return  
-end
-
+    return  
+    end#meansMahalinobisKernel
 
 end#MeansMahalinobis
 
