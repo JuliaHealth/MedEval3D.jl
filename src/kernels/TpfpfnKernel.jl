71e7d6e3404bd87aa58b82a97ea9bf77ad1a28ab
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2021-11-10 14:16:20 +0100

    up

--- src/kernels/TpfpfnKernel.jl
+++ src/kernels/TpfpfnKernel.jl
@@ -66,69 +66,92 @@ function getBlockTpFpFn(goldBoolGPU#goldBoolGPU
         , segmBoolGPU#segmBoolGPU
         ,sliceMetricsTupl
         ,tp,tn,fp,fn#tp,tn,fp,fn
-        ,loopNumb#loopNumb
-        ,pixelNumberPerSlice#pixelNumberPerSlice
+        ,arrDims,loopXdim,loopYdim,loopZdim,
         ,numberToLooFor#numberToLooFor
         ,conf)
-    #offset for lloking for values in source arrays 
-        offset = (pixelNumberPerSlice*(blockIdx().x-1))
     
-#creates shared memory and initializes it to 0
-   shmemSum = createAndInitializeShmem(threadIdxX(),threadIdxY())
-   sync_threads()
-# incrementing appropriate number of times 
+    
+    shmemSum = @cuStaticSharedMem(UInt32, (33,3))  
     locArr= zeros(MVector{3,UInt16})
-
-    @unroll for k in UInt16(0):loopNumb
-        if(threadIdxX()+(threadIdxY()-1)*32+k*1024 <=pixelNumberPerSlice)
-           ind =offset+ threadIdxX()+(threadIdxY()-1)*32+k*1024
+      @iter3dAdditionalzActs(arrDims,loopXdim,loopYdim,loopZdim,
+    #inner expression
+    begin
+        #updating variables needed to calculate means
            boolGold = goldBoolGPU[ind]==numberToLooFor  
            boolSegm = segmBoolGPU[ind]==numberToLooFor     
              @inbounds locArr[ (boolGold & boolSegm + boolSegm +1) ]+=(boolGold | boolSegm)
-        end#if 
-    end#for
+    end,
+    #after z expression - we get slice wise true counts from it 
+    begin         end ) 
 
-   offsetIter = UInt16(1)
-    while(offsetIter <32) 
-      @inbounds locArr[3]+=shfl_down_sync(FULL_MASK, locArr[3], offsetIter)  
-      @inbounds locArr[2]+=shfl_down_sync(FULL_MASK, locArr[2], offsetIter)  
-      @inbounds locArr[1]+=shfl_down_sync(FULL_MASK, locArr[1], offsetIter)  
-      offsetIter<<= 1
-    end
-    #shmemSum[threadIdxX(),3]+=locArr[3]
-    if(threadIdxX()==1)
-      @inbounds shmemSum[threadIdxY(),3]+=locArr[3]
-      @inbounds shmemSum[threadIdxY(),2]+=locArr[2]
-      @inbounds shmemSum[threadIdxY(),1]+=locArr[1]
-    end
-
+    #tell what variables are to be reduced and by what operation
+    @redWitAct(offsetIter,shmemSum,  locArr[1],+,     locArr[2],+,     locArr[3],+   )
     sync_threads()
-    if(threadIdxY()==1)
-      offsetIter = UInt16(1)
-      while(offsetIter <32) 
-        @inbounds shmemSum[threadIdxX(),3]+=shfl_down_sync(FULL_MASK, shmemSum[threadIdxX(),3], offsetIter)  
-        @inbounds shmemSum[threadIdxX(),2]+=shfl_down_sync(FULL_MASK, shmemSum[threadIdxX(),2], offsetIter)  
-        @inbounds shmemSum[threadIdxX(),1]+=shfl_down_sync(FULL_MASK, shmemSum[threadIdxX(),1], offsetIter)  
-        offsetIter<<= 1
-      end
-    end  
-#now we have needed values in  shmemSum[1,2] shmemSum[1,3] and shmemSum[1,1]
-sync_threads()
-#no point in calculating anything if we have 0 
-if((shmemSum[1,3] + shmemSum[1,2] +shmemSum[1,1]) >0)
-  @ifXY 1 1  @inbounds @atomic tp[]+= shmemSum[1,3]
-  @ifXY 1 2  @inbounds @atomic fp[]+= shmemSum[1,2]
-  @ifXY 1 3  @inbounds @atomic fn[]+= shmemSum[1,1]
-#calculated if we are intrewested in given slice wise metrics
-  if(conf.sliceWiseMatrics)
-    @ifXY 1 4  @inbounds sliceMetricsTupl[1][blockIdxX()]=shmemSum[1,3]
-    @ifXY 1 5  @inbounds sliceMetricsTupl[2][blockIdxX()]=shmemSum[1,2]
-    @ifXY 1 6  @inbounds sliceMetricsTupl[3][blockIdxX()]=shmemSum[1,1]
+    @sendAtomicHelperAndAdd(shmemSum, fn, fp,tp)
+    #   @ifXY 1 1  @inbounds @atomic tp[]+= shmemSum[1,3]
+#   @ifXY 1 2  @inbounds @atomic fp[]+= shmemSum[1,2]
+#   @ifXY 1 3  @inbounds @atomic fn[]+= shmemSum[1,1]
     
-    getMetrics(shmemSum[1,3], shmemSum[1,2], shmemSum[1,1] , pixelNumberPerSlice-(shmemSum[1,3] + shmemSum[1,2] +shmemSum[1,1]) ,sliceMetricsTupl,conf ,blockIdxX())
+    
+#     #offset for lloking for values in source arrays 
+#         offset = (pixelNumberPerSlice*(blockIdx().x-1))
+    
+# #creates shared memory and initializes it to 0
+#    shmemSum = createAndInitializeShmem(threadIdxX(),threadIdxY())
+#    sync_threads()
+# # incrementing appropriate number of times 
+#     locArr= zeros(MVector{3,UInt16})
 
-  end#if
-end#if
+#     @unroll for k in UInt16(0):loopNumb
+#         if(threadIdxX()+(threadIdxY()-1)*32+k*1024 <=pixelNumberPerSlice)
+#            ind =offset+ threadIdxX()+(threadIdxY()-1)*32+k*1024
+#            boolGold = goldBoolGPU[ind]==numberToLooFor  
+#            boolSegm = segmBoolGPU[ind]==numberToLooFor     
+#              @inbounds locArr[ (boolGold & boolSegm + boolSegm +1) ]+=(boolGold | boolSegm)
+#         end#if 
+#     end#for
+
+#    offsetIter = UInt16(1)
+#     while(offsetIter <32) 
+#       @inbounds locArr[3]+=shfl_down_sync(FULL_MASK, locArr[3], offsetIter)  
+#       @inbounds locArr[2]+=shfl_down_sync(FULL_MASK, locArr[2], offsetIter)  
+#       @inbounds locArr[1]+=shfl_down_sync(FULL_MASK, locArr[1], offsetIter)  
+#       offsetIter<<= 1
+#     end
+#     #shmemSum[threadIdxX(),3]+=locArr[3]
+#     if(threadIdxX()==1)
+#       @inbounds shmemSum[threadIdxY(),3]+=locArr[3]
+#       @inbounds shmemSum[threadIdxY(),2]+=locArr[2]
+#       @inbounds shmemSum[threadIdxY(),1]+=locArr[1]
+#     end
+
+#     sync_threads()
+#     if(threadIdxY()==1)
+#       offsetIter = UInt16(1)
+#       while(offsetIter <32) 
+#         @inbounds shmemSum[threadIdxX(),3]+=shfl_down_sync(FULL_MASK, shmemSum[threadIdxX(),3], offsetIter)  
+#         @inbounds shmemSum[threadIdxX(),2]+=shfl_down_sync(FULL_MASK, shmemSum[threadIdxX(),2], offsetIter)  
+#         @inbounds shmemSum[threadIdxX(),1]+=shfl_down_sync(FULL_MASK, shmemSum[threadIdxX(),1], offsetIter)  
+#         offsetIter<<= 1
+#       end
+#     end  
+# #now we have needed values in  shmemSum[1,2] shmemSum[1,3] and shmemSum[1,1]
+# sync_threads()
+# #no point in calculating anything if we have 0 
+# if((shmemSum[1,3] + shmemSum[1,2] +shmemSum[1,1]) >0)
+#   @ifXY 1 1  @inbounds @atomic tp[]+= shmemSum[1,3]
+#   @ifXY 1 2  @inbounds @atomic fp[]+= shmemSum[1,2]
+#   @ifXY 1 3  @inbounds @atomic fn[]+= shmemSum[1,1]
+# #calculated if we are intrewested in given slice wise metrics
+#   if(conf.sliceWiseMatrics)
+#     @ifXY 1 4  @inbounds sliceMetricsTupl[1][blockIdxX()]=shmemSum[1,3]
+#     @ifXY 1 5  @inbounds sliceMetricsTupl[2][blockIdxX()]=shmemSum[1,2]
+#     @ifXY 1 6  @inbounds sliceMetricsTupl[3][blockIdxX()]=shmemSum[1,1]
+    
+#     getMetrics(shmemSum[1,3], shmemSum[1,2], shmemSum[1,1] , pixelNumberPerSlice-(shmemSum[1,3] + shmemSum[1,2] +shmemSum[1,1]) ,sliceMetricsTupl,conf ,blockIdxX())
+
+#   end#if
+# end#if
 
    return  
    end
