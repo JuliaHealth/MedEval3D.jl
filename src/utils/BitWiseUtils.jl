e719f0115ddb0e3f17de29b560ba8cbca496ac69
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2021-11-24 17:48:11 +0100

    up

--- src/utils/BitWiseUtils.jl
+++ src/utils/BitWiseUtils.jl
@@ -7,14 +7,22 @@ module BitWiseUtils
 """
 sets given bit of supplied number to 1
 """
-macro setBitTo1(offsetIter,z)
+macro setBitTo1(numb,position)
     return esc(quote
 
     end)
   
 end#setBitTo1
+"""
+set bit of number numb in position pos to value val
+"""
+macro setBitTo(numb,pos,val)
+    return esc(quote
 
-
+    end)
+  
+end#setBitTo1
+setBitTo
 
 
 end#BitWiseUtils
