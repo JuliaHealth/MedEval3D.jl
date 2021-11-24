4d96528b7670ee72dc4d663a6b1d9650f70078cc
Jakub Mitura <53857487+jakubMitura14@users.noreply.github.com>
2021-11-24 16:26:23 +0100

    Create BitWiseUtils.jl

---
+++ src/utils/BitWiseUtils.jl
@@ -0,0 +1,20 @@
+"""
+makes some bitwise operations easier
+"""
+
+module BitWiseUtils
+
+"""
+sets given bit of supplied number to 1
+"""
+macro setBitTo1(offsetIter,z)
+    return esc(quote
+
+    end)
+  
+end#setBitTo1
+
+
+
+
+end#BitWiseUtils
